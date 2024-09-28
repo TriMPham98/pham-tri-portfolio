e26eacdae572277ed77895b6ea90b6a8b1e7beeb
Tri <trimpham98@gmail.com>
2024-09-27 23:07:06 -0700

    Change to night theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,23 @@ import { Projects } from "@/components/Projects";
 import { Skills } from "@/components/Skills";
 import { Contact } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
+import RetroGrid from "@/components/ui/retro-grid";
 
 export default function Portfolio() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-grow">
-        <Hero />
-        <About />
-        <Projects />
-        <Skills />
-        <Contact />
-      </main>
-      <Footer />
+    <div className="min-h-screen flex flex-col relative">
+      <RetroGrid className="fixed inset-0" />
+      <div className="relative z-10">
+        <Header />
+        <main className="flex-grow">
+          <Hero />
+          <About />
+          <Projects />
+          <Skills />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
