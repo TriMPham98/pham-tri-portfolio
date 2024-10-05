7082c70dafb4310eac2cf2a98efbfb2c750330fa
Tri <trimpham98@gmail.com>
2024-10-04 21:20:17 -0700

    Replace subheader placeholder text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,8 +21,7 @@ export function Hero() {
           },
         }}>
         <p className="mt-4 md:mt-6 text-lg md:text-xl text-gray-300 max-w-2xl mx-auto px-4 md:px-0">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          I connect humans and machines through user interfaces.
         </p>
       </AnimateOnScroll>
     </section>
