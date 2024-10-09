cbbefa0f9477edbeb2605f766cf8e9f8cffa6d44
Tri <trimpham98@gmail.com>
2024-10-09 12:11:07 -0700

    Add on hover animation for rainbow button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -33,8 +33,13 @@ export function Hero() {
             I design user interfaces that connect humans and machines.
           </p>
           <div className="flex justify-center">
-            <Link href="/files/Tri-Pham-Resume.pdf" target="_blank" rel="noopener noreferrer">
-              <RainbowButton>View Résumé</RainbowButton>
+            <Link
+              href="/files/Tri-Pham-Resume.pdf"
+              target="_blank"
+              rel="noopener noreferrer">
+              <RainbowButton className="hover:scale-105">
+                View Résumé
+              </RainbowButton>
             </Link>
           </div>
         </AnimateOnScroll>
