42392dee6b09a34c11804658d5ab0abbf6490bd8
Tri <trimpham98@gmail.com>
2024-10-09 14:55:51 -0700

    Resolve type errors

--- src/components/ui/particle-links.tsx
+++ src/components/ui/particle-links.tsx
@@ -3,68 +3,67 @@
 import React, { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadLinksPreset } from "@tsparticles/preset-links";
+import type { ISourceOptions, Engine } from "@tsparticles/engine";
 
 const ParticleLinks = () => {
   const [init, setInit] = useState(false);
 
   useEffect(() => {
-    initParticlesEngine(async (engine) => {
+    initParticlesEngine(async (engine: Engine) => {
       await loadLinksPreset(engine);
     }).then(() => {
       setInit(true);
     });
   }, []);
 
+  const options: ISourceOptions = {
+    preset: "links",
+    background: {
+      color: "transparent",
+    },
+    particles: {
+      number: {
+        value: 100,
+        density: {
+          enable: true,
+        },
+      },
+      color: {
+        value: "#ffffff",
+      },
+      links: {
+        enable: true,
+        color: "#ffffff",
+        opacity: 0.3,
+      },
+      move: {
+        enable: true,
+        speed: 1,
+      },
+      size: {
+        value: { min: 1, max: 3 },
+      },
+    },
+    interactivity: {
+      events: {
+        onHover: {
+          enable: true,
+          mode: "grab",
+        },
+      },
+      modes: {
+        grab: {
+          distance: 140,
+          links: {
+            opacity: 0.5,
+          },
+        },
+      },
+    },
+  };
+
   if (init) {
-    return (
-      <Particles
-        id="tsparticles"
-        options={{
-          preset: "links",
-          particles: {
-            color: {
-              value: "#ffffff",
-            },
-            links: {
-              color: "#ffffff",
-              opacity: 0.3,
-            },
-            move: {
-              speed: 1,
-            },
-            size: {
-              value: { min: 1, max: 3 },
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 169,
-            },
-          },
-          background: {
-            color: "transparent",
-          },
-          interactivity: {
-            events: {
-              onHover: {
-                enable: true,
-                mode: "grab",
-              },
-            },
-            modes: {
-              grab: {
-                distance: 140,
-                links: {
-                  opacity: 0.5,
-                },
-              },
-            },
-          },
-        }}
-      />
-    );
+    return <Particles id="tsparticles" options={options} />;
   }
 
   return null;
