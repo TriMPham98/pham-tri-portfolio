76ae19e4781278af892f12466f63e8de3ce65962
Tri <trimpham98@gmail.com>
2024-10-09 14:26:15 -0700

    Replace img tag with Image component

--- src/components/ui/avatar-circles.tsx
+++ src/components/ui/avatar-circles.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-
+import Image from "next/image";
 import { cn } from "@/lib/utils";
 
 interface AvatarCirclesProps {
@@ -18,19 +18,20 @@ const AvatarCircles = ({
   return (
     <div className={cn("z-10 flex -space-x-4 rtl:space-x-reverse", className)}>
       {avatarUrls.map((url, index) => (
-        <img
+        <div
           key={index}
-          className="h-10 w-10 rounded-full border-2 border-white dark:border-gray-800"
-          src={url}
-          width={40}
-          height={40}
-          alt={`Avatar ${index + 1}`}
-        />
+          className="relative h-10 w-10 rounded-full border-2 border-white dark:border-gray-800 overflow-hidden">
+          <Image
+            src={url}
+            alt={`Avatar ${index + 1}`}
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
       ))}
       <a
         className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-white bg-black text-center text-xs font-medium text-white hover:bg-gray-600 dark:border-gray-800 dark:bg-white dark:text-black"
-        href=""
-      >
+        href="">
         +{numPeople}
       </a>
     </div>
