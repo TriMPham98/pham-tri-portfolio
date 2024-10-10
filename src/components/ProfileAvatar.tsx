4fd54bab9322b2c953989b9cddcdd4557743a3d0
Tri <trimpham98@gmail.com>
2024-10-10 07:52:20 -0700

    Allow component to accept className prop

--- src/components/ProfileAvatar.tsx
+++ src/components/ProfileAvatar.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 
-export default function ProfileAvatar() {
+interface ProfileAvatarProps {
+  className?: string;
+}
+
+const ProfileAvatar: React.FC<ProfileAvatarProps> = ({ className }) => {
   return (
-    <Avatar className="w-40 h-40 sm:w-48 sm:h-48 md:w-64 md:h-64 lg:w-72 lg:h-72 border-4 border-white">
+    <Avatar
+      className={`w-40 h-40 sm:w-48 sm:h-48 md:w-64 md:h-64 lg:w-72 lg:h-72 border-4 border-white ${
+        className || ""
+      }`}>
       <AvatarImage
         src="/images/TriGuitarHeadshot.jpg"
         alt="Tri's profile picture"
@@ -12,4 +19,6 @@ export default function ProfileAvatar() {
       <AvatarFallback>TP</AvatarFallback>
     </Avatar>
   );
-}
+};
+
+export default ProfileAvatar;
