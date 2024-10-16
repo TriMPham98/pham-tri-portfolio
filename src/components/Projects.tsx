50f979dbb245dad5e57b5be6ed85793acf8294b9
Tri <trimpham98@gmail.com>
2024-10-15 22:38:45 -0700

    Remove h3 subheader text

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -145,9 +145,6 @@ export function Projects() {
         </BentoGrid>
         {/* Fun Projects Section */}
         <div className="mt-16">
-          <h3 className="text-2xl font-bold text-white text-center mb-8">
-            Fun Projects
-          </h3>
           <Marquee className="py-4 bg-gradient-to-r from-gray-900 to-black border border-gray-800 rounded-lg">
             {funProjects.map((project, index) => (
               <a
