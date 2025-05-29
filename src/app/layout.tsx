815d33a90931dde018f57928b4880af5783c88b9
Tri <trimpham98@gmail.com>
2025-05-29 09:40:14 -0700

    Add Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -32,6 +33,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning={true}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
